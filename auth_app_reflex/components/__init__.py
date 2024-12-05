c71d3fdd593c74c99ff4319e55a69d0bf4887d3d
florinhegedus <florinxhgds@gmail.com>
2024-12-05 17:34:20 +0200

    removed components module

--- auth_app_reflex/components/__init__.py
+++
@@ -1,8 +0,0 @@
-from .login_components import login_single_thirdparty
-from .register_components import register_single_thirdparty
-
-
-__all__ = [
-    'login_single_thirdparty',
-    'register_single_thirdparty',
-]
--- auth_app_reflex/pages/login.py
+++ auth_app_reflex/pages/login.py
@@ -1,7 +1,118 @@
 import reflex as rx
 
 from .. import navigation
-from ..components import login_single_thirdparty
+from ..auth import AuthState
+
+
+def login_single_thirdparty() -> rx.Component:
+    return rx.card(
+        rx.vstack(
+            rx.flex(
+                rx.image(
+                    src="/logo.png",
+                    width="2.5em",
+                    height="auto",
+                    border_radius="25%",
+                ),
+                rx.heading(
+                    "Sign in to your account",
+                    size="6",
+                    as_="h2",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.hstack(
+                    rx.text(
+                        "New here?",
+                        size="3",
+                        text_align="left",
+                    ),
+                    rx.link("Sign up", href=navigation.routes.REGISTER_ROUTE, size="3"),
+                    spacing="2",
+                    opacity="0.8",
+                    width="100%",
+                ),
+                direction="column",
+                justify="start",
+                spacing="4",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.text(
+                    "Email address",
+                    size="3",
+                    weight="medium",
+                    text_align="left",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("user")),
+                    placeholder="user@reflex.dev",
+                    on_blur=AuthState.set_username,
+                    type="email",
+                    size="3",
+                    width="100%",
+                ),
+                justify="start",
+                spacing="2",
+                width="100%",
+            ),
+            rx.vstack(
+                rx.hstack(
+                    rx.text(
+                        "Password",
+                        size="3",
+                        weight="medium",
+                    ),
+                    rx.link(
+                        "Forgot password?",
+                        href=navigation.routes.RESET_PASSWORD,
+                        size="3",
+                    ),
+                    justify="between",
+                    width="100%",
+                ),
+                rx.input(
+                    rx.input.slot(rx.icon("lock")),
+                    placeholder="Enter your password",
+                    on_blur=AuthState.set_password,
+                    type="password",
+                    size="3",
+                    width="100%",
+                ),
+                spacing="2",
+                width="100%",
+            ),
+            rx.button("Sign in", 
+                      on_click=AuthState.login(),
+                      size="3", 
+                      width="100%"),
+            rx.hstack(
+                rx.divider(margin="0"),
+                rx.text(
+                    "Or continue with",
+                    white_space="nowrap",
+                    weight="medium",
+                ),
+                rx.divider(margin="0"),
+                align="center",
+                width="100%",
+            ),
+            rx.button(
+                rx.icon(tag="mail"),
+                "Sign in with Google",
+                on_click=navigation.NavState.to_home(),
+                variant="outline",
+                size="3",
+                width="100%",
+            ),
+            spacing="6",
+            width="100%",
+        ),
+        size="4",
+        max_width="28em",
+        width="100%",
+    )
 
 
 def login_page() -> rx.Component:
