9fab40b251e86bf59cfe0b2585d7cf366c3f5e70
florinhegedus <florinxhgds@gmail.com>
2024-12-07 16:58:48 +0200

    refactoring of encrypt

--- auth_app_reflex/auth/encrypt.py
+++ auth_app_reflex/auth/encrypt.py
@@ -2,13 +2,13 @@ import bcrypt
 
 
 def hash_password(password: str) -> str:
-    # Generate a salt
+    """ Generate hash for the given password. """
     salt = bcrypt.gensalt()
-    # Hash the password
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')  # Convert bytes to string
+    return hashed_password.decode('utf-8')
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """ Compare current password with the saved encrypted password. """
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
