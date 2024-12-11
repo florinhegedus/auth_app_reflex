a6d64f99f47ae59eccef219d30fd266b83880e79
florinhegedus <florinxhgds@gmail.com>
2024-12-11 15:57:43 +0200

    fix DeprecationWarning for GoogleLogin component

--- auth_app_reflex/google_auth/react_oauth_google.py
+++ auth_app_reflex/google_auth/react_oauth_google.py
@@ -8,8 +8,12 @@ class GoogleOAuthProvider(rx.Component):
     client_id: rx.Var[str]
 
 
+def _on_success_signature(data: rx.Var[dict]) -> tuple[rx.Var[dict]]:
+    return (data, )
+
+
 class GoogleLogin(rx.Component):
     library = "@react-oauth/google"
     tag = "GoogleLogin"
 
-    on_success: rx.EventHandler[lambda data: [data]]
+    on_success: rx.EventHandler[_on_success_signature]
